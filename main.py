698cbab0d875c7ae7f3e85f86d33459a472d0560
Bhupesh-V <varshneybhupesh@gmail.com>
2023-02-18 19:03:20 +0530

    ♻️ add meta tags across pages

--- main.py
+++ main.py
@@ -16,25 +16,31 @@ class Config:
 
 
 class FilterLevelForm(FlaskForm):
-    level = SelectField("level", choices=[
-        ("beginner", "beginner"),
-        ("intermediate", "intermediate"),
-        ("advanced", "advanced"),
-        ("everyone", "everyone"),
-    ])
+    level = SelectField(
+        "level",
+        choices=[
+            ("beginner", "beginner"),
+            ("intermediate", "intermediate"),
+            ("advanced", "advanced"),
+            ("everyone", "everyone"),
+        ],
+    )
 
 
 class FilterTypeForm(FlaskForm):
-    type = SelectField("type", choices=[
-        ("video", "video"),
-        ("article", "article"),
-        ("book", "book"),
-        ("github", "github"),
-        ("course", "course"),
-        ("audio", "audio"),
-        ("cheatsheet", "cheatsheet"),
-        ("website", "website"),
-    ])
+    type = SelectField(
+        "type",
+        choices=[
+            ("video", "video"),
+            ("article", "article"),
+            ("book", "book"),
+            ("github", "github"),
+            ("course", "course"),
+            ("audio", "audio"),
+            ("cheatsheet", "cheatsheet"),
+            ("website", "website"),
+        ],
+    )
 
 
 app = Flask(__name__)
@@ -44,8 +50,8 @@ CATEGORIES = {
     "Python": "python",
     "JavaScript": "javascript",
     "C++": "cpp",
-    "C":"c",
-    "CSS":"css",
+    "C": "c",
+    "CSS": "css",
     "Data Structures & Algorithms": "dsa",
     "Machine Learning": "machine-learning",
     "Deep Learning": "deep-learning",
@@ -59,27 +65,41 @@ CATEGORIES = {
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("categories.html", categories=CATEGORIES)
+    title = "Saadhan by r/developersIndia"
+    return render_template("categories.html", categories=CATEGORIES, title=title)
 
 
 @app.route("/resources", methods=["GET"])
 def category():
     category = request.args.get("category")
+
     rsr = resourcer.Resource(category)
     resources = rsr.get_resource()
+
     level_form = FilterLevelForm()
     type_form = FilterTypeForm()
+
     return render_template(
-        "resources.html", resources=resources["resources"], category=category, level_form=level_form, type_form=type_form
+        "resources.html",
+        resources=resources["resources"],
+        category=category,
+        level_form=level_form,
+        type_form=type_form,
+        title=f"All {category} resources on saadhan - r/developersIndia",
     )
 
 
 @app.route("/contributors", methods=["GET"])
 def contributors():
     contributors = resourcer.Resource.get_all_contributors()
-    saadhan__contributors = resourcer.Resource.get_saadhan_contributors()
+    saadhan_contributors = resourcer.Resource.get_saadhan_contributors()
+
+    title = f"Saadhan is possible by more than {len(contributors)+len(saadhan_contributors)} community contributors 🤝 - r/developersIndia"
     return render_template(
-        "contributors.html", contributors=contributors["contributors"]+saadhan__contributors["contributors"]
+        "contributors.html",
+        contributors=contributors["contributors"]
+        + saadhan_contributors["contributors"],
+        title=title,
     )
 
 
